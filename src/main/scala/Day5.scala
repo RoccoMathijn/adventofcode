d58522fe32fa3a0545fc913ace1a55cc1ae3e79a
rmathijn <rocco.mathijnandela@dhl.com>
2018-12-06 00:06:54 +0100

    Some cleanup

--- src/main/scala/Day5.scala
+++ src/main/scala/Day5.scala
@@ -6,24 +6,22 @@ object Day5 extends App {
     .fromResource("input-day5.txt")
     .toList
 
-  def react(polymer: List[Char], acc: List[Char]): List[Char] = {
-    (polymer, acc) match {
-      case (Nil, _) => acc
-      case (c1 :: cs, Nil) => react(cs, List(c1))
-      case (c1 :: cs, c2 :: as) =>
-        if (c1 != c2 && c1.toLower == c2.toLower) react(cs, as)
-        else react(cs, c1 :: acc)
+  def react(polymer: List[Char]): List[Char] = {
+    polymer.foldRight(List.empty[Char]) { (c1, acc) =>
+      val c2Option = acc.headOption
+      if (c2Option.exists(c2 => c1 != c2 && c1.toLower == c2.toLower)) acc.tail
+      else c1 +: acc
     }
   }
 
-  val reactedPolymer = react(polymer, List.empty)
+  val reactedPolymer = react(polymer)
 
   // Part1
   println(s"Answer Part 1: ${reactedPolymer.length}")
 
   // Part2
   val shortestPolymerAfterRemovingOneUnit: Int = ('a' to 'z').map(unit =>
-    react(reactedPolymer.filterNot(c => c.toLower == unit), List.empty).length
+    react(reactedPolymer.filterNot(c => c.toLower == unit)).length
   ).min
 
   val endTime = System.currentTimeMillis()
