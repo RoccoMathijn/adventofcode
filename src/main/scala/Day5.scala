08325678d777d31df0739d7eac814ca3c3152f8f
rmathijn <rocco.mathijnandela@dhl.com>
2018-12-05 23:26:05 +0100

    More performance improvements

--- src/main/scala/Day5.scala
+++ src/main/scala/Day5.scala
@@ -5,28 +5,28 @@ import scala.io.Source
 object Day5 extends App {
   val polymer = Source
     .fromResource("input-day5.txt")
-    .getLines()
-    .toList
-    .head
+    .getLines
+    .mkString
 
   def react(polymer: String): String = {
-    polymer.foldLeft(" ")((acc, c) =>
-      if ((acc.last.isUpper == c.isLower && acc.last.toLower == c) || (acc.last.isLower == c.isUpper && acc.last.toUpper == c)) acc.init
-      else acc :+ c).trim
+    polymer.foldRight(" ")((c, acc) => {
+      val head = acc.head
+      if (head != c && head.toLower == c.toLower) acc.tail
+      else c +: acc
+    }).trim
   }
 
   val reactedPolymer = react(polymer)
 
   // Part1
-  val lengthAfterReaction: Int = reactedPolymer.length
-  println(lengthAfterReaction)
+  println(reactedPolymer.length)
 
   // Part2
-  val shortestPolymerAfterRemovingOneUnit: Future[Int] = Future.sequence(('a' to 'z').map { unit =>
+  val shortestPolymerAfterRemovingOneUnit: Future[Int] = Future.sequence(('a' to 'z').map (unit =>
     Future {
       react(reactedPolymer.filterNot(c => c.toLower == unit)).length
     }
-  }).map(_.min)
+  )).map(_.min)
 
   shortestPolymerAfterRemovingOneUnit onComplete println
 
