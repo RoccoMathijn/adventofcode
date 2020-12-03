811be51bb76155f933badf9a4138a7ba773c3ac7
rmathijn <rm.andela@gmail.com>
2020-12-03 09:47:29 +0100

    cleanup

--- src/main/scala/aoc2020/Day03.scala
+++ src/main/scala/aoc2020/Day03.scala
@@ -2,7 +2,7 @@ package aoc2020
 
 import scala.collection.immutable
 import scala.io.Source
-import scala.util.control.Breaks._
+
 object Day03 extends App {
   val input: immutable.Seq[String] = Source
     .fromResource("aoc2020/input-day3.txt")
@@ -12,31 +12,28 @@ object Day03 extends App {
   val stepX = 1
   val stepY = 2
 
-  val mapX = input.head.length
-  val mapY = input.length
-
   def isTree(x: Int, y: Int): Boolean = {
     input(y)(x) == '#'
   }
 
   def nextStep(x: Int, y: Int): (Int, Int) = {
-    val newX = (x + stepX) % mapX
+    val newX = (x + stepX) % input.head.length
     val newY = y + stepY
 
     (newX, newY)
   }
 
+  var finished = false
   var count = 0
   var nStep = (0, 0)
 
-  while (true) {
-    if (nStep._2 >= mapY) {
-      print(count)
-      break
-    }
-    if (isTree(nStep._1, nStep._2)) {
+  while (!finished) {
+    if (nStep._2 >= input.length) {
+      finished = true
+    } else if (isTree(nStep._1, nStep._2)) {
       count += 1
     }
     nStep = nextStep(nStep._1, nStep._2)
   }
+  print(count)
 }
