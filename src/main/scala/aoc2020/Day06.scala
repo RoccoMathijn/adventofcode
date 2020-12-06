96905cadfc8f76ee9c449d9a17ab6821cd7ec991
rmathijn <rm.andela@gmail.com>
2020-12-06 06:18:48 +0100

    Day 6

--- src/main/scala/aoc2020/Day06.scala
+++ src/main/scala/aoc2020/Day06.scala
@@ -1,14 +1,17 @@
 package aoc2020
 
-import scala.collection.immutable
 import scala.io.Source
 
 object Day06 extends App {
-  val input: immutable.Seq[Int] = Source
+  val input: List[List[List[String]]] = Source
     .fromResource("aoc2020/input-day6.txt")
     .getLines()
-    .map(_.toInt)
+    .mkString("\n")
+    .split("\n\n")
+    .map(_.split("\n").map(_.grouped(1).toList).toList)
     .toList
 
+  println(input.map(_.flatten.distinct.size).sum)
 
+  println(input.map(_.reduce(_ intersect _)).map(_.size).sum)
 }
