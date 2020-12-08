0eb7a5871f3ab29e657b15342066601a11cbaddf
rmathijn <rm.andela@gmail.com>
2020-12-08 07:35:38 +0100

    Day8

--- src/main/scala/aoc2020/Day08.scala
+++ src/main/scala/aoc2020/Day08.scala
@@ -1,14 +1,44 @@
 package aoc2020
 
-import scala.collection.immutable
+import scala.annotation.tailrec
+import scala.collection.mutable.ListBuffer
 import scala.io.Source
 
 object Day08 extends App {
-  val input: immutable.Seq[Int] = Source
+  val input: Seq[Instruction] = Source
     .fromResource("aoc2020/input-day8.txt")
     .getLines()
-    .map(_.toInt)
     .toList
+    .map { l =>
+      val split = l.split(' ')
+      Instruction(split(0), split(1).toInt)
+    }
 
+  case class Instruction(operation: String, argument: Int)
 
+  @tailrec
+  def run(input: Seq[Instruction], history: List[Int] = List.empty, pointer: Int = 0, acc: Int = 0): Either[Int, Int] = {
+    if (pointer == input.size) Right(acc)
+    else if (history.contains(pointer)) Left(acc)
+    else {
+      input(pointer).operation match {
+        case "nop" => run(input, history :+ pointer, pointer + 1, acc)
+        case "acc" => run(input, history :+ pointer, pointer + 1, acc + input(pointer).argument)
+        case "jmp" => run(input, history :+ pointer, pointer + input(pointer).argument, acc)
+      }
+    }
+  }
+
+  var permutations = new ListBuffer[Seq[Instruction]].empty
+  for (i <- input.indices) {
+    val instruction: Instruction = input(i)
+    instruction.operation match {
+      case "jmp" => permutations += input.patch(i, List(Instruction("nop", instruction.argument)), 1)
+      case "nop" => permutations += input.patch(i, List(Instruction("jmp", instruction.argument)), 1)
+      case _     =>
+    }
+  }
+
+  println(run(input))
+  println(permutations.map(run(_)).filter(_.isRight))
 }
