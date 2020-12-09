ef1e1b77831b7cec1882fd798c59e31c0b97882c
rmathijn <rm.andela@gmail.com>
2020-12-09 06:41:32 +0100

    Day 09 - initial approach

--- src/main/scala/aoc2020/Day09.scala
+++ src/main/scala/aoc2020/Day09.scala
@@ -5,12 +5,39 @@ import aoc2020.InputGetter._
 object Day09 extends AocTools(9) {
 //  implicit private val mode: Mode = Example
   implicit private val mode: Mode = Live
-  inputLines
+  val preambleLength = 25
+
+  def sumNext(target: Long, contigious: Seq[Long], pointer: Int): Option[Seq[Long]] = {
+    if (contigious.sum == target) Some(contigious)
+    else if (contigious.sum > target) None
+    else sumNext(target, contigious :+ inputLongs(mode)(pointer), pointer + 1)
+  }
+
   def main(args: Array[String]): Unit = {
     println(s"AOC 2020 - Day $day")
-    val part1 = ???
-    val part2 = ???
+
+    val part1 = inputLongs.view.zipWithIndex
+      .drop(preambleLength)
+      .find { x =>
+        val preamble = inputLongs.slice(x._2 - preambleLength, x._2)
+        val sums = preamble.flatMap { y =>
+          preamble.map { x =>
+            x + y
+          }
+        }
+        !sums.contains(x._1)
+      }
+      .map(_._1)
+
     println(s"Answer part 1: $part1")
+
+    val part2: Option[Long] = inputLongs.indices
+      .map(sumNext(part1.get, Seq.empty, _))
+      .find(_.isDefined)
+      .flatMap(_.map { contigious =>
+        contigious.min + contigious.max
+      })
+
     println(s"Answer part 2: $part2")
   }
 }
