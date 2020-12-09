9c072e09b9812520fe5e5a12342682ce1411b87e
rmathijn <rm.andela@gmail.com>
2020-12-09 06:42:44 +0100

    Day 09 - speedup

--- src/main/scala/aoc2020/Day09.scala
+++ src/main/scala/aoc2020/Day09.scala
@@ -7,19 +7,21 @@ object Day09 extends AocTools(9) {
   implicit private val mode: Mode = Live
   val preambleLength = 25
 
+  val input = inputLongs
+
   def sumNext(target: Long, contigious: Seq[Long], pointer: Int): Option[Seq[Long]] = {
     if (contigious.sum == target) Some(contigious)
     else if (contigious.sum > target) None
-    else sumNext(target, contigious :+ inputLongs(mode)(pointer), pointer + 1)
+    else sumNext(target, contigious :+ input(pointer), pointer + 1)
   }
 
   def main(args: Array[String]): Unit = {
     println(s"AOC 2020 - Day $day")
 
-    val part1 = inputLongs.view.zipWithIndex
+    val part1 = input.view.zipWithIndex
       .drop(preambleLength)
       .find { x =>
-        val preamble = inputLongs.slice(x._2 - preambleLength, x._2)
+        val preamble = input.slice(x._2 - preambleLength, x._2)
         val sums = preamble.flatMap { y =>
           preamble.map { x =>
             x + y
@@ -31,7 +33,7 @@ object Day09 extends AocTools(9) {
 
     println(s"Answer part 1: $part1")
 
-    val part2: Option[Long] = inputLongs.indices
+    val part2: Option[Long] = input.indices
       .map(sumNext(part1.get, Seq.empty, _))
       .find(_.isDefined)
       .flatMap(_.map { contigious =>
