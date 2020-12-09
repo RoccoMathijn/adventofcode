4dbddf3d96fcd2608433ff046eafa1109f7e9eba
rmathijn <rm.andela@gmail.com>
2020-12-09 06:54:28 +0100

    Scaffold day 10

---
+++ src/main/scala/aoc2020/Day10.scala
@@ -0,0 +1,19 @@
+import aoc2020.InputGetter.{Live, Mode}
+import aoc2020.{AocTools, InputGetter}
+
+object Day10 extends AocTools(10) {
+  //  implicit private val mode: Mode = Example
+  implicit private val mode: Mode = Live
+
+  val input = inputInts
+
+  def main(args: Array[String]): Unit = {
+    println(s"AOC 2020 - Day $day")
+
+    val part1 = ???
+    println(s"Answer part 1: $part1")
+
+    //    val part2 = ???
+    //    println(s"Answer part 2: $part2")
+  }
+}
