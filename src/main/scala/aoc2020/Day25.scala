dcb26650dfb912b8796f9511365b5ac143438bd6
rmathijn <rm.andela@gmail.com>
2020-12-25 12:00:42 +0100

    AOC 2020 - Day25

--- src/main/scala/aoc2020/Day25.scala
+++ src/main/scala/aoc2020/Day25.scala
@@ -1,7 +1,7 @@
 package aoc2020
 
 import util.AocTools
-import util.InputGetter.{Live, Mode}
+import util.InputGetter.{Example, Live, Mode}
 
 import scala.annotation.tailrec
 
@@ -9,18 +9,32 @@ object Day25 extends AocTools(25, 2020) {
 //  implicit private val mode: Mode = Example
   implicit private val mode: Mode = Live
 
-  val input: Seq[String] = inputLines
+  val input: Seq[Int] = inputLines.map(_.toInt)
+  val cardPubKey = input(0)
+  val doorPubKey = input(1)
+
+  val cardLoopsize = findLoopSize(cardPubKey, passthroughValue = 1L)
+  val doorLoopsize = findLoopSize(doorPubKey, passthroughValue = 1L)
+
+  def transform(loopSize: Int, subject: Long, value: Long): Long = {
+    (1 to loopSize).foldLeft(value) { (newValue, _) => (newValue * subject) % 20201227 }
+  }
+
+  def findLoopSize(pubKey: Int, loopSize: Int = 0, passthroughValue: Long): Long = {
+    val initialValue = transform(loopSize = 1, subject = 7, value = passthroughValue)
+    if (pubKey == initialValue) loopSize + 1
+    else {
+      findLoopSize(pubKey, loopSize + 1, initialValue)
+    }
+  }
 
   def main(args: Array[String]): Unit = {
     val start = System.currentTimeMillis()
     println(s"AOC 2020 - Day $day")
 
-    val part1 = ???
+    val cardSecret = transform(doorLoopsize.toInt, cardPubKey, 1)
+    val part1 = cardSecret
     val mid = System.currentTimeMillis()
     println(s"Answer part 1: $part1 [${mid - start}ms]")
-
-    val part2 = ???
-    val end = System.currentTimeMillis()
-    println(s"Answer part 2: $part2 [${`end` - mid}ms]")
   }
 }
