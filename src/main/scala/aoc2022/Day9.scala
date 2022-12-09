c022a415b0d5707a35edd184657dc6a1e0a750ec
rmathijn <rm.andela@gmail.com>
2022-12-09 15:11:27 +0100

    AOC 2022 - Day 9

--- src/main/scala/aoc2022/Day9.scala
+++ src/main/scala/aoc2022/Day9.scala
@@ -8,82 +8,54 @@ object Day9 extends AocTools(9, 2022) {
   implicit private val mode: Mode = Live
 
   case class Pos(x: Int, y: Int)
-  case class History(h: List[Pos], tails: Map[String, List[Pos]])
+  case class State(rope: List[Pos], tailHistory: Set[Pos])
 
-  val s = List(Pos(0, 0))
-  val tailMap = Map(
-    "1" -> s,
-    "2" -> s,
-    "3" -> s,
-    "4" -> s,
-    "5" -> s,
-    "6" -> s,
-    "7" -> s,
-    "8" -> s,
-    "9" -> s
-  )
-  def history: History =
-    inputLines.foldLeft(History(s, tailMap)) {
-      case (History(h :: hx, tailMap), s"R $x") =>
+  def buildHistory(ropeSize: Int): State =
+    inputLines.foldLeft(State(List.fill(ropeSize)(Pos(0, 0)), Set.empty)) {
+      case (State(h :: body, history), s"R $x") =>
         val hPath = (1 to x.toInt).map(x => h.copy(x = h.x + x)).toList
-        History(hPath.reverse ++ (h :: hx), buildTMap(hPath, tailMap))
-      case (History(h :: hx, tailMap), s"L $x") =>
+        val bodyPaths = motion(hPath, body)
+        State(hPath.last :: bodyPaths.head, history ++ bodyPaths.map(_.last))
+      case (State(h :: body, history), s"L $x") =>
         val hPath = (1 to x.toInt).map(x => h.copy(x = h.x - x)).toList
-        History(hPath.reverse ++ (h :: hx), buildTMap(hPath, tailMap))
-      case (History(h :: hx, tailMap), s"U $y") =>
+        val bodyPaths = motion(hPath, body)
+        State(hPath.last :: bodyPaths.head, history ++ bodyPaths.map(_.last))
+      case (State(h :: body, history), s"U $y") =>
         val hPath = (1 to y.toInt).map(y => h.copy(y = h.y + y)).toList
-        History(hPath.reverse ++ (h :: hx), buildTMap(hPath, tailMap))
-      case (History(h :: hx, tailMap), s"D $y") =>
+        val bodyPaths = motion(hPath, body)
+        State(hPath.last :: bodyPaths.head, history ++ bodyPaths.map(_.last))
+      case (State(h :: body, history), s"D $y") =>
         val hPath = (1 to y.toInt).map(y => h.copy(y = h.y - y)).toList
-        History(hPath.reverse ++ (h :: hx), buildTMap(hPath, tailMap))
+        val bodyPaths = motion(hPath, body)
+        State(hPath.last :: bodyPaths.head, history ++ bodyPaths.map(_.last))
     }
 
-  def buildTMap(hPath: List[Pos], tailMap: Map[String, List[Pos]]): Map[String, List[Pos]] = {
-    val t1 = buildTPath(hPath, tailMap("1").head)
-    val t2 = buildTPath(t1.reverse, tailMap("2").head)
-    val t3 = buildTPath(t2.reverse, tailMap("3").head)
-    val t4 = buildTPath(t3.reverse, tailMap("4").head)
-    val t5 = buildTPath(t4.reverse, tailMap("5").head)
-    val t6 = buildTPath(t5.reverse, tailMap("6").head)
-    val t7 = buildTPath(t6.reverse, tailMap("7").head)
-    val t8 = buildTPath(t7.reverse, tailMap("8").head)
-    val t9 = buildTPath(t8.reverse, tailMap("9").head)
+  def motion(headPath: List[Pos], body: List[Pos]): List[List[Pos]] =
+    headPath.foldLeft(List(body))((bodyHistory, head) => step(head, bodyHistory.head) :: bodyHistory)
 
-    Map(
-      "1" -> (t1 ++ tailMap("1")),
-      "2" -> (t2 ++ tailMap("2")),
-      "3" -> (t3 ++ tailMap("3")),
-      "4" -> (t4 ++ tailMap("4")),
-      "5" -> (t5 ++ tailMap("5")),
-      "6" -> (t6 ++ tailMap("6")),
-      "7" -> (t7 ++ tailMap("7")),
-      "8" -> (t8 ++ tailMap("8")),
-      "9" -> (t9 ++ tailMap("9"))
-    )
-  }
+  def step(headPosition: Pos, body: List[Pos]): List[Pos] =
+    body
+      .foldLeft(List(headPosition)) { (newBody, follower) =>
+        follow(newBody.head, follower) :: newBody
+      }
+      .reverse
+      .drop(1)
 
-  def buildTPath(predecessor: List[Pos], tStart: Pos): List[Pos] = {
-    predecessor.foldLeft(List(tStart)) {
-      case (lnt :: lntx, newH) =>
-        (follow(newH, lnt) ++ (lnt :: lntx)).toList
-    }
-  }
-
-  def follow(hPos: Pos, tPos: Pos): Option[Pos] = {
+  def follow(hPos: Pos, tPos: Pos): Pos = {
     val xDistance = math.abs(hPos.x - tPos.x)
     val yDistance = math.abs(hPos.y - tPos.y)
     (xDistance, yDistance) match {
-      case (2, 0) => Some(Pos((hPos.x + tPos.x) / 2, tPos.y)) // move horizontal
-      case (0, 2) => Some(Pos(tPos.x, (hPos.y + tPos.y) / 2)) // move vertically
-      case (2, 1) => Some(Pos((hPos.x + tPos.x) / 2, hPos.y)) // move diagonally on x axis
-      case (1, 2) => Some(Pos(hPos.x, (hPos.y + tPos.y) / 2)) // move diagonally on y axis
-      case (2, 2) => Some(Pos((hPos.x + tPos.x) / 2, (hPos.y + tPos.y) / 2)) // move diagonally on both axes
-      case _      => None
+      case (2, 0) => Pos((hPos.x + tPos.x) / 2, tPos.y) // move horizontal
+      case (0, 2) => Pos(tPos.x, (hPos.y + tPos.y) / 2) // move vertically
+      case (2, 1) => Pos((hPos.x + tPos.x) / 2, hPos.y) // move diagonally on x axis
+      case (1, 2) => Pos(hPos.x, (hPos.y + tPos.y) / 2) // move diagonally on y axis
+      case (2, 2) => Pos((hPos.x + tPos.x) / 2, (hPos.y + tPos.y) / 2) // move diagonally on both axes
+      case _      => tPos
     }
   }
 
-  def solve1: Int = history.tails("1").distinct.size
-  def solve2: Int = history.tails("9").distinct.size
+  def solve1: Int = buildHistory(2).tailHistory.size
+  def solve2: Int = buildHistory(10).tailHistory.size
 
   def main(args: Array[String]): Unit = {
     val start = System.currentTimeMillis()
