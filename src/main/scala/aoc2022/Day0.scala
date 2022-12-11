c963c27f23eb457d57065395968519bad4bf4e3f
rmathijn <rm.andela@gmail.com>
2022-12-11 20:15:32 +0100

    AOC 2022 - template

--- src/main/scala/aoc2022/Day0.scala
+++ src/main/scala/aoc2022/Day0.scala
@@ -7,14 +7,17 @@ object Day0 extends AocTools(0, 2022) {
   //    implicit private val mode: Mode = Example
   implicit private val mode: Mode = Live
 
+  def solve1 = ???
+  def solve2 = ???
+  
   def main(args: Array[String]): Unit = {
     val start = System.currentTimeMillis()
     println(s"AOC 2022 - Day $day")
 
-    val part1 = ""  
+    val part1 = solve1  
     val mid = System.currentTimeMillis()
     println(s"Answer part 1: $part1 [${mid - start}ms]")
-    val part2 = ""
+    val part2 = solve2
     val end = System.currentTimeMillis()
     println(s"Answer part 2: $part2 [${end - mid}ms]")
   }
