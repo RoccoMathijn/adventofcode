52c545f939c5b9680854b3b8359255edcfa4a421
rmathijn <rm.andela@gmail.com>
2022-12-18 14:16:46 +0100

    AOC 2022 - day 18 - start with one block

--- src/main/scala/aoc2022/Day18.scala
+++ src/main/scala/aoc2022/Day18.scala
@@ -1,75 +1,61 @@
 package aoc2022
 
 import util.AocTools
-import util.InputGetter.{Live, Mode}
+import util.InputGetter.{Example, Live, Mode}
 
 object Day18 extends AocTools(18, 2022) {
-//      implicit private val mode: Mode = Example
+//  implicit private val mode: Mode = Example
   implicit private val mode: Mode = Live
 
-  case class Cube(x: Int, y: Int, z: Int)
+  case class Voxel(x: Int, y: Int, z: Int)
   val lavaDroplet = inputLines.map { line =>
     val Array(x, y, z) = line.split(',')
-    Cube(x.toInt, y.toInt, z.toInt)
+    Voxel(x.toInt, y.toInt, z.toInt)
   }.toSet
-  
-  private def surfaceArea(cubes: Set[Cube]): Int = {
-    (6 * cubes.size) - cubes.toList.map { cube =>
-      cubes.count { otherCube => (math.abs(otherCube.x - cube.x) + math.abs(otherCube.y - cube.y) + math.abs(otherCube.z - cube.z)) == 1 }
+
+  private def surfaceArea(shape: Set[Voxel]): Int = {
+    (6 * shape.size) - shape.toList.map { cube =>
+      shape.count { otherCube => (math.abs(otherCube.x - cube.x) + math.abs(otherCube.y - cube.y) + math.abs(otherCube.z - cube.z)) == 1 }
     }.sum
   }
 
-  def neighbours(cube: Cube): Set[Cube] = {
-    Set(cube.copy(x = cube.x - 1), cube.copy(x = cube.x + 1), cube.copy(y = cube.y - 1), cube.copy(y = cube.y + 1), cube.copy(z = cube.z - 1), cube.copy(z = cube.z + 1))
+  def neighbours(cube: Voxel): Set[Voxel] = {
+    Set(
+      cube.copy(x = cube.x - 1),
+      cube.copy(x = cube.x + 1),
+      cube.copy(y = cube.y - 1),
+      cube.copy(y = cube.y + 1),
+      cube.copy(z = cube.z - 1),
+      cube.copy(z = cube.z + 1)
+    )
   }
 
-  def outOfBounds(cube: Cube): Boolean = cube.x < xMin || cube.x > xMax || cube.y < yMin || cube.y > yMax || cube.z < zMin || cube.z > zMax
-  
-  def expand(air: Set[Cube]): Set[Cube] = {
-    val newAir = air.flatMap(neighbours).diff(lavaDroplet).filterNot(outOfBounds)
+  def outOfBounds(cube: Voxel): Boolean = cube.x < xMin || cube.x > xMax || cube.y < yMin || cube.y > yMax || cube.z < zMin || cube.z > zMax
+
+  def expand(air: Set[Voxel]): Set[Voxel] = {
+    val newAir = air ++ air.flatMap(neighbours).diff(lavaDroplet).filterNot(outOfBounds)
     if (newAir == air) air
     else expand(newAir)
   }
-  
+
   private val xMin: Int = lavaDroplet.map(_.x).min - 1
   private val xMax: Int = lavaDroplet.map(_.x).max + 1
-  val xAxis = xMin to xMax
   private val yMin: Int = lavaDroplet.map(_.y).min - 1
   private val yMax: Int = lavaDroplet.map(_.y).max + 1
-  val yAxis = yMin to yMax
   private val zMin: Int = lavaDroplet.map(_.z).min - 1
   private val zMax: Int = lavaDroplet.map(_.z).max + 1
-  val zAxis = zMin to zMax
-
-  val outerLayerAir: Set[Cube] = {
-    val bottom = (for {
-      x <- xAxis
-      z <- zAxis
-    } yield Cube(x, 0, z)).toSet
-
-    val left = (for {
-      y <- yAxis
-      z <- zAxis
-    } yield Cube(0, y, z)).toSet
-
-    val front = (for {
-      x <- xAxis
-      y <- yAxis
-    } yield Cube(x, y, 0)).toSet
-
-    bottom ++ left ++ front ++ bottom.map(_.copy(y = yAxis.max)) ++ bottom.map(_.copy(x = xAxis.max)) ++ front.map(_.copy(z = zAxis.max))
-  }
+  private val genesis = Voxel(xMin, yMin, zMin)
 
   val all = (for {
-    x <- xAxis
-      y<-yAxis
-    z<-zAxis
-  } yield Cube(x, y, z)).toSet
+    x <- xMin to xMax
+    y <- yMin to yMax
+    z <- zMin to zMax
+  } yield Voxel(x, y, z)).toSet
 
   def solve1: Int = surfaceArea(lavaDroplet)
 
   def solve2: Int = {
-    val expanded = expand(outerLayerAir) 
+    val expanded = expand(Set(genesis))
     solve1 - surfaceArea(all -- lavaDroplet -- expanded)
   }
 
